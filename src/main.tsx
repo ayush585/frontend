b7aaf9af14a03a5edfac982ad627a2e8685f47f5
ayush585 <ayushmanmukherjee12@gmail.com>
2025-09-03 20:40:54 +0530

    Delete doc

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,12 @@ import './index.css'
 
 // import your two pages
 import Landing from './App'
-import Docs from './Docs'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/docs" element={<Docs />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
